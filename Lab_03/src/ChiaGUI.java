fa2514e6f68fabb6662e75404adfe644f1f453b3
zannguyen <nguyen2406gl@gmail.com>
2025-04-10 09:11:14 +0700

    Update

---
+++ Lab_03/src/ChiaGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ChiaGUI extends JFrame {
+    private JTextField txtSo1, txtSo2, txtKetQua;
+
+    public ChiaGUI() {
+        
+    }
+
+    private void tinhTong() {
+        
+    }
+}
